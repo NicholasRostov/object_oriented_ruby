c260d6e5e68ee646b02ef0833535188120b58b16
Nicholas Rostov <jonathan.w.coll@gmail.comgit config --global user.email jonathan.w.coll@gmail.com>
2017-01-09 20:02:57 -0500

    My cereal store

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+# I am a store that sells cereal
+# 3 items
+# Minimum 3 properties
+
+captain_munch = {sugar_content: "24mlg", color: "Purple", milk_fusion: "mild"}
+cinnamon_waffle = { :sugar_content => "50mlg", :color => "Brown", :milk_fusion => "extreme"}
+apple_franks = {sugar_content: "20mlg", color: "Red/Blue", milk_fusion: "none"}
